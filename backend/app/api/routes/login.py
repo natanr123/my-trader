b702003e4fe25d939f4bb7e34791ca74d029f63f
natan <natanr123@gmail.com>
2025-09-28 19:48:41 +0300

    dgdfgdgf

--- backend/app/api/routes/login.py
+++ backend/app/api/routes/login.py
@@ -11,7 +11,8 @@ from app.core import security
 from app.core.config import settings
 from app.core.security import get_password_hash
 from app.models.models import Message
-from app.models.user import NewPassword, Token, UserPublic
+from app.models.user import UserPublic
+from app.models.token import NewPassword, Token
 from app.utils import (
     generate_password_reset_token,
     generate_reset_password_email,
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -55,18 +55,3 @@ class UsersPublic(SQLModel):
     data: list[UserPublic]
     count: int
 
-
-# JSON payload containing access token
-class Token(SQLModel):
-    access_token: str
-    token_type: str = "bearer"
-
-
-# Contents of JWT token
-class TokenPayload(SQLModel):
-    sub: str | None = None
-
-
-class NewPassword(SQLModel):
-    token: str
-    new_password: str = Field(min_length=8, max_length=40)
