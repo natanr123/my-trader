7abe34aa8be6d0c223eccbdddfdfdba297928a6a
natan <natanr123@gmail.com>
2025-09-28 23:26:14 +0300

    dgdfgdgf

--- backend/app/api/deps/alpaca_dep.py
+++ backend/app/api/deps/alpaca_dep.py
@@ -4,7 +4,6 @@ from app.utils.my_alpaca_client import MyAlpacaClient
 from app.core.config.alpaca_settings import alpaca_settings
 
 def get_alpaca_client():
-    print('alpaca_settings.credentialsalpaca_settings.credentialsalpaca_settings.credentials: ', alpaca_settings.credentials)
     return MyAlpacaClient(alpaca_settings.credentials)
 
 AlpacaDep = Annotated[MyAlpacaClient, Depends(get_alpaca_client)]
