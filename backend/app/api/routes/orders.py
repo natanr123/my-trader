bd9ccfca2aaadfbb17a7d9b098cff7a0a2d17175
natan <natanr123@gmail.com>
2025-09-28 18:21:42 +0300

    dgdfgdgf

--- backend/app/api/routes/orders.py
+++ backend/app/api/routes/orders.py
@@ -86,76 +86,10 @@ def sync_orders(
     results = session.exec(statement)
     orders = results.all()
     total_orders = len(orders)
-    # my_file_log.append('syncing orders... total_orders=', total_orders)
 
     print('Total orders: ', total_orders)
     for order in orders:
-        if order.alpaca_buy_order_id:
-            alpaca_buy_order = alpaca_client.get_order_by_id(order.alpaca_buy_order_id)
-        else:
-            raise Exception('Order was created but no matching alpaca buy order')
-
-        alpaca_sell_order: AlpacaOrder | None = None
-        if order.alpaca_sell_order_id:
-            alpaca_sell_order = alpaca_client.get_order_by_id(order.alpaca_sell_order_id)
-
-        # my_file_log.append('working on order id=', order.id, 'status=', order.status, 'alpaca_status=', alpaca_buy_order.status, 'alpaca_sell_order=', bool(alpaca_sell_order))
-
-        #NEW -> BUY_PENDING_NEW -> BUY_ACCEPTED -> BUY_FILLED -> SELL_PENDING_NEW -> SELL_ACCEPTED -> SELL_FILLED
-        if order.status == VirtualOrderStatus.BUY_PENDING_NEW:
-            print('the order id=', order.id, 'waiting for it to be filled. Nothing to do for now')
-            if alpaca_buy_order.status == AlpacaOrderStatus.ACCEPTED:
-                # my_file_log.append('the order id=', order.id, 'moving from buying to accepted')
-                order.buy_accepted()
-                session.commit()
-            elif alpaca_buy_order.status == AlpacaOrderStatus.FILLED:
-                # my_file_log.append('the order id=', order.id, 'moving from buying to filled')
-                order.buy_accepted()
-                session.commit()
-                session.refresh(order)
-                market_close_at = alpaca_client.get_next_close()
-                order.buy_filled(filled_avg_price=alpaca_buy_order.filled_avg_price, buy_filled_qty=alpaca_buy_order.filled_qty, market_close_at=market_close_at)
-                session.commit()
-        elif order.status == VirtualOrderStatus.BUY_ACCEPTED:
-            if alpaca_buy_order.status == AlpacaOrderStatus.ACCEPTED:
-                print('the order id=', order.id, 'waiting for it to be filled. Nothing to do for now')
-            elif alpaca_buy_order.status == AlpacaOrderStatus.FILLED:
-                # my_file_log.append('the order id=', order.id, 'is moving from buying accepted buying to filled')
-                market_close_at = alpaca_client.get_next_close()
-                order.buy_filled(filled_avg_price=alpaca_buy_order.filled_avg_price, buy_filled_qty=alpaca_buy_order.filled_qty, market_close_at=market_close_at)
-                session.commit()
-        elif order.status == VirtualOrderStatus.BUY_FILLED:
-            still_have_time = not alpaca_client.is_time_passed(order.force_sell_at)
-            if still_have_time:
-                # my_file_log.append('the order id=', order.id, 'was buy filled','but it is not time-passed', 'doing nothing')
-                continue
-            ######################################################
-            try:
-                alpaca_sell_order = alpaca_client.close_position(order.symbol)
-                # my_file_log.append('the order id=', order.id, 'buy was filled', 'sell position not found', 'sending sell order -> buy_pending_new')
-                order.sell_submitted(alpaca_order_id=alpaca_sell_order.id)
-            except APIError as e:
-                # my_file_log.append('the order id=', order.id, 'buy was filled', 'sell position arelady FOUND', 'skipping to sell filled')
-                order.sell_submitted(alpaca_order_id=None)
-                order.sell_accepted()
-                order.sell_filled(filled_avg_price=0, filled_qty=0)
-
-            session.commit()
-            ############################################
-        elif order.status == VirtualOrderStatus.SELL_PENDING_NEW:
-            if alpaca_sell_order.status == AlpacaOrderStatus.ACCEPTED:
-                # my_file_log.append('the order id=', order.id, 'sell order was accepted')
-                order.sell_accepted()
-                session.commit()
-            elif alpaca_sell_order.status == AlpacaOrderStatus.FILLED:
-                # my_file_log.append('the order id=', order.id, 'sell order was filled')
-                order.sell_accepted()
-                order.sell_filled(filled_avg_price=alpaca_buy_order.filled_avg_price, filled_qty=alpaca_buy_order.filled_qty)
-                session.commit()
-
-
-
-
+        order.sync(alpaca_client=alpaca_client)
     return {"result": "success"}
 
 
