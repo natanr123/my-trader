d2c355496f96088fe7dccf1a6f92bca3f0f3baca
natan <natanr123@gmail.com>
2025-09-30 00:39:31 +0300

    dfgdfgdfg

--- backend/tests/services/test_order_service.py
+++ backend/tests/services/test_order_service.py
@@ -2,14 +2,35 @@ from app.clients.my_alpaca_client import MyAlpacaClient
 from sqlmodel import Session
 from app.services.order_service import OrderService
 from app.models.order import OrderCreate
+from app.models.user import UserCreate, User
+from app.crud import create_user
 
 def test_handle_buy_pending_new(alpaca_client: MyAlpacaClient, order_service: OrderService, db: Session) -> None:
     print('alpaca_clientalpaca_clientalpaca_client: ', alpaca_client)
     print('order_serviceorder_serviceorder_service: ', order_service)
 
     print('aaaaaaaaaaaaaaaaaa')
+    user_create = UserCreate(email='aaaa@aaa.com', password='12345678')
+    user = create_user(session=db, user_create=user_create)
+    order_in = OrderCreate(symbol='AAPL', amount=100)
+    order_service.create_order_with_alpaca_order(user=user, order_in=order_in, session=db, alpaca_client=alpaca_client)
 
-    order = OrderCreate()
+
+    # db_obj = User.model_validate(
+    #     user_create, update={"hashed_password": get_password_hash(user_create.password)}
+    # )
+    # session.add(db_obj)
+    # session.commit()
+    # session.refresh(db_obj)
+
+    # user_create = UserCreate(email='aaaaaaa@aaaa.com', password='12345678')
+    # user = User.model_validate(user_create, update = {"hashed_password": get_password_hash(user_create.password)})
+    # db.add(user)
+    # db.commit()
+    # db.refresh(user)
+    # print(user)
+
+    # order = order_service.create_order_with_alpaca_order()
 
 
     # order_service._handle_buy_pending_new()
