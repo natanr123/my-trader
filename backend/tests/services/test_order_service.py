ad6e7d68d345d0f9e47dcda920b791fc120fa61a
natan <natanr123@gmail.com>
2025-09-30 01:44:16 +0300

    dfgdfgdfg

--- backend/tests/services/test_order_service.py
+++ backend/tests/services/test_order_service.py
@@ -2,19 +2,10 @@ from app.clients.my_alpaca_client import MyAlpacaClient, AlpacaOrder, AlpacaOrde
 from sqlmodel import Session
 from app.services.order_service import OrderService, OrderSyncData
 from app.models.order import OrderCreate, OrderPublic, Order, VirtualOrderStatus
-from app.models.user import UserCreate, User
-from app.crud import create_user
 from uuid import UUID
 from datetime import datetime
-def test_handle_buy_pending_new(alpaca_client: MyAlpacaClient, order_service: OrderService, db: Session) -> None:
-    print('alpaca_clientalpaca_clientalpaca_client: ', alpaca_client)
-    print('order_serviceorder_serviceorder_service: ', order_service)
 
-    print('aaaaaaaaaaaaaaaaaa')
-    user_create = UserCreate(email='aaaa@aaa.com', password='12345678')
-    user = create_user(session=db, user_create=user_create)
-    order_in = OrderCreate(symbol='AAPL', amount=100)
-    # order_service.create_order_with_alpaca_order(user=user, order_in=order_in, session=db, alpaca_client=alpaca_client)
+def test_handle_buy_pending_new(alpaca_client: MyAlpacaClient, order_service: OrderService, db: Session) -> None:
     order = Order(status=VirtualOrderStatus.BUY_PENDING_NEW)
     buy_order = AlpacaOrder(id=UUID('cbe2c370-f613-41d0-9833-248f1ade5d6d'),
                             client_order_id="",
@@ -28,23 +19,5 @@ def test_handle_buy_pending_new(alpaca_client: MyAlpacaClient, order_service: Or
     sync_data = OrderSyncData(buy_order=buy_order, sell_order=None)
 
     order_service._handle_buy_pending_new(order=order, sync_data=sync_data, alpaca_client=alpaca_client)
+    assert order.status == VirtualOrderStatus.BUY_ACCEPTED
 
-
-    # db_obj = User.model_validate(
-    #     user_create, update={"hashed_password": get_password_hash(user_create.password)}
-    # )
-    # session.add(db_obj)
-    # session.commit()
-    # session.refresh(db_obj)
-
-    # user_create = UserCreate(email='aaaaaaa@aaaa.com', password='12345678')
-    # user = User.model_validate(user_create, update = {"hashed_password": get_password_hash(user_create.password)})
-    # db.add(user)
-    # db.commit()
-    # db.refresh(user)
-    # print(user)
-
-    # order = order_service.create_order_with_alpaca_order()
-
-
-    # order_service._handle_buy_pending_new()
