fd5ed2ac040f251ad36702733bea6f277577cc92
natan <natanr123@gmail.com>
2025-09-30 11:58:29 +0300

    dfgdfgdfg

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -43,8 +43,8 @@ class UpdatePassword(SQLModel):
 class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     hashed_password: str
-    items: list["app.models.item.Item"] = Relationship(back_populates="owner", cascade_delete=True)
-    orders: list["app.models.order.Order"] = Relationship(back_populates="owner", cascade_delete=True)
+    items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)  # type: ignore[name-defined]
+    orders: list["Order"] = Relationship(back_populates="owner", cascade_delete=True)  # type: ignore[name-defined]
 
 
 # Properties to return via API, id is always required
