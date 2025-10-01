fc51a0bb15a3aba19e2a987c608f52a1d938ec2e
natan <natanr123@gmail.com>
2025-10-01 19:37:02 +0300

    dfgdfgdfg

---
+++ backend/tests/models/test_order.py
@@ -0,0 +1,23 @@
+from app.clients.my_alpaca_client import MyAlpacaClient, AlpacaOrder, AlpacaOrderStatus
+from sqlmodel import Session
+from app.models.order import Order, VirtualOrderStatus
+from uuid import UUID
+from app.models.user import UserCreate
+from app.crud import crud
+from app.models.order import OrderCreate
+
+def test_buy_submitted(alpaca_client: MyAlpacaClient, db: Session) -> None:
+    user_in = UserCreate(email='test@test.com', password='12345678')
+    user = crud.create_user(session=db, user_create=user_in)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    alpaca_order_id = UUID('cbe2c370-f613-41d0-9833-248f1ade5d6d')
+    order_in = OrderCreate(symbol='AAPL', amount=55)
+    order = Order.model_validate(order_in, update={"owner_id": user.id})
+    assert order.status == VirtualOrderStatus.NEW
+    order.buy_submitted(alpaca_order_id=alpaca_order_id)
+    db.add(order)
+    db.commit()
+    db.refresh(order)
+    assert order.status == VirtualOrderStatus.BUY_PENDING_NEW
