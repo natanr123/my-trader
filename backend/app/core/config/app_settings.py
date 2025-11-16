c15b62422100db69fdb491fb748922037fc8d8b5
natan <natanr123@gmail.com>
2025-11-16 19:49:19 +0200

    ghjghjghj

--- backend/app/core/config/app_settings.py
+++ backend/app/core/config/app_settings.py
@@ -86,15 +86,6 @@ class Settings(BaseSettings):
             else:
                 raise ValueError(message)
 
-    @model_validator(mode="after")
-    def _enforce_non_default_secrets(self) -> Self:
-        self._check_default_secret("SECRET_KEY", self.SECRET_KEY)
-        self._check_default_secret(
-            "FIRST_SUPERUSER_PASSWORD", self.FIRST_SUPERUSER_PASSWORD
-        )
-
-        return self
-
 
 app_settings = Settings()  # type: ignore
 
