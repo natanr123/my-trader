cd17e98bfc9fc058d1e0a3f8d02350dd530972cd
natan <natanr123@gmail.com>
2025-11-17 13:19:51 +0200

    ruff

--- backend/app/commands/alpaca-ws.py
+++ backend/app/commands/alpaca-ws.py
@@ -1,9 +1,11 @@
 from app.api.deps.alpaca_dep import get_my_alpaca_client
 from app.clients.my_alpaca_client import AlpacaBar
+import logging
+logger = logging.getLogger(__name__)
 
 
 async def on_amd_bar(bar: AlpacaBar) -> None:
-    print(
+    logger.info(
         f"New AMD 1m bar: "
         f"{bar.timestamp} "
         f"o={bar.open} h={bar.high} l={bar.low} c={bar.close} v={bar.volume}"
@@ -11,5 +13,6 @@ async def on_amd_bar(bar: AlpacaBar) -> None:
 
 
 my_alpaca_client = get_my_alpaca_client()
+logger.info("Subscribing to AMD 1m bars...")
 my_alpaca_client.subscribe_bar_crypto(pair="BTC/USD", on_bar=on_amd_bar)
 
