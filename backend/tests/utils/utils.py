8d206114d3b1c1fab72de40c6a0e337a9ea54b83
natan <natanr123@gmail.com>
2025-11-17 13:42:38 +0200

    test

--- backend/tests/utils/utils.py
+++ backend/tests/utils/utils.py
@@ -4,6 +4,7 @@ import string
 from fastapi.testclient import TestClient
 
 from app.core.config.app_settings import app_settings
+from app.core.config.super_user_settings import super_user_settings
 
 
 def random_lower_string() -> str:
@@ -16,8 +17,8 @@ def random_email() -> str:
 
 def get_superuser_token_headers(client: TestClient) -> dict[str, str]:
     login_data = {
-        "username": app_settings.FIRST_SUPERUSER,
-        "password": app_settings.FIRST_SUPERUSER_PASSWORD,
+        "username": super_user_settings.FIRST_SUPER_USER_EMAIL,
+        "password": super_user_settings.FIRST_SUPER_USER_PASSWORD,
     }
     r = client.post(f"{app_settings.API_V1_STR}/login/access-token", data=login_data)
     tokens = r.json()
